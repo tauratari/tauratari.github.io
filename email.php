387521c37ea879ba4704adfc4823c5a5d94065ac
tauratari <74375401+tauratari@users.noreply.github.com>
2020-11-26 19:43:20 +0100

    Update email.php

--- email.php
+++ email.php
@@ -1,6 +1,6 @@
 <?php
 if(isset($_POST['submit'])) {
-$to = "you@you.com";
+$to = "venicequeenonmercury@gmail.com";
 $subject = "Form Tutorial";
 $name_field = $_POST['name'];
 $email_field = $_POST['email'];

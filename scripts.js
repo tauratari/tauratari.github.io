afc0d07b52c26f502fc707b4f1fdebd175f4b7c9
tauratari <74375401+tauratari@users.noreply.github.com>
2020-11-26 19:39:54 +0100

    Delete scripts.js

--- scripts.js
+++
@@ -1,9 +0,0 @@
-/* Toggle between adding and removing the "responsive" class to topnav when the user clicks on the icon */
-function header() {
-  var x = document.getElementById("mainmenu");
-  if (x.className === "menu") {
-    x.className += " responsive";
-  } else {
-    x.className = "menu";
-  }
-}
